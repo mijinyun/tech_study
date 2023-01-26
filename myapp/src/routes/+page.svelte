c67d11a741660db7e089f897de345e94f3f4dd30
mijinyun <yunmj1224@gmail.com>
2023-01-26 22:27:30 +0900

    study: variable, styling, insert string including html element

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,5 +1,29 @@
 <script>
+// 1.introduce
     let name = 'world';
+    let image = '/image.gif'
+    import Nested from "./Nested.svelte";
+    let string = `This includes <strong style="color:blue">html section</strong>.`;
 </script>
 
+<style>
+/* introduce */
+    h3 {
+        color:crimson;
+        font-size:2rem;
+    }
+
+    h3 > div {
+        color:purple;
+    }
+</style>
+
+<!-- 1. introduce -->
 <h1>hello {name.toUpperCase()}!</h1>
+<img src={image} alt="dancing!">
+<h3>Styling test!
+    <p>sub testing</p>
+    <div>sub testing2</div>
+</h3>
+<Nested />
+<p>{@html string}</p>
