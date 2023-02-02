ad097cab349f30522cb4053bb2020413d375393b
mijinyun <yunmj1224@gmail.com>
2023-02-02 18:28:15 +0900

    study: modify routing ex

--- myapp/src/routes/blog/+page.svelte
+++ myapp/src/routes/blog/+page.svelte
@@ -1,16 +1,11 @@
 <script>
-    export let data;
+	export let data;
 </script>
 
 <h1>blog</h1>
 
 <ul>
-    <!-- <li><a href="/part2/blog/one">one</a></li>
-    <li><a href="/part2/blog/two">two</a></li>
-    <li><a href="/part2/blog/three">three</a></li> -->
-
-    {#each data.summaries as { slug, title }}
-        <li><a href="/blog/{slug}">{title}</a></li>
-    
-    {/each}
+	{#each data.summaries as { slug, title }}
+		<li><a href="/blog/{slug}">{title}</a></li>
+	{/each}
 </ul>
--- myapp/src/routes/blog/[slug]/+page.server.js
+++ myapp/src/routes/blog/[slug]/+page.server.js
@@ -1,13 +1,12 @@
 import { error } from '@sveltejs/kit';
-import { posts } from '../data.js'
+import { posts } from '../data.js';
 
 export function load({ params }) {
+	const post = posts.find((post) => post.slug === params.slug);
 
-    const post = posts.find((post) => {
-        post.slug == params.slug;
-    })
+	if (!post) throw error(404);
 
-    if (!post) throw error(404);
-
-    return { post };
+	return {
+		post
+	};
 }
